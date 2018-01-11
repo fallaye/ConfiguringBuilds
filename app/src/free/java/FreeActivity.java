588f2d76d1bdc275b7203bfd11581e777c346f9a
fallaye <fallaye.diallo@gmail.com>
2018-01-11 08:43:51 -0500

    Initial commit

---
+++ app/src/free/java/FreeActivity.java
@@ -0,0 +1,6 @@
+/**
+ * Created by fallaye on 1/10/18.
+ */
+
+public class FreeActivity {
+}
---
+++ app/src/main/java/mobileappscompany/configuringbuilds/sample/SampleMain.java
@@ -0,0 +1,12 @@
+package mobileappscompany.configuringbuilds.sample;
+
+/**
+ * Created by fallaye on 1/10/18.
+ */
+
+public class SampleMain {
+
+    public  void testMethod(){
+
+    }
+}
